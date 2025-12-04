694f165d3f87ca9702735bb7f0257391c84343f0
georgebullock <george.bullock@gmx.de>
2025-12-04 17:18:34 +0100

    Revert default Prisma client generation. Add first-pass at create user route. Add password generator util stub

--- 1_code_first/begin/backend/src/index.ts
+++ 1_code_first/begin/backend/src/index.ts
@@ -1,9 +1,13 @@
 import express from "express";
-import { Request, Response } from "express";
+import { Request, Response, NextFunction } from "express";
+import { PrismaClient } from "@prisma/client";
 const app = express();
 const port = 3000;
+const prisma = new PrismaClient();
 
-app.get("/", (req: Request, res: Response) => {
+app.use(express.json());
+
+app.get("/", (req: Request, res: Response, next: NextFunction) => {
   console.log(
     "Hey 👋🏿. Welcome to the Software Essentialist Backend API project!"
   );
@@ -12,10 +16,35 @@ app.get("/", (req: Request, res: Response) => {
   );
 });
 
-app.post("/users/new", (req: Request, res: Response) => {
-  console.log("Create new user");
-  return res.send("Create new user");
-});
+app.post(
+  "/users/new",
+  async (req: Request, res: Response, next: NextFunction) => {
+    // Get data from the request
+    const { email, username, password, firstName, lastName, id } = req.body;
+
+    try {
+      // Try to add data to the database
+      await prisma.user.create({
+        data: {
+          id,
+          email,
+          username,
+          firstName,
+          lastName,
+          password,
+        },
+      });
+
+      res.send();
+    } catch (err) {
+      next(err);
+      console.log(err);
+      // If failure return error 409 | 400 | 500 with the right message
+    }
+
+    // Maybe do something else here...
+  }
+);
 
 app.post("/users/edit/:userId ", (req: Request, res: Response) => {
   console.log("Get user by ID");
