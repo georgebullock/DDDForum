093c91e2913520e3c31a58a542b2bddd0226e4fa
georgebullock <george.bullock@gmx.de>
2026-01-10 12:04:39 +0100

    👌 IMPROVE: Remove unnecessary index log

--- 1_code_first/begin/backend/src/api/routes/indexRoute.ts
+++ 1_code_first/begin/backend/src/api/routes/indexRoute.ts
@@ -3,9 +3,6 @@ import { Router, Request, Response, NextFunction } from "express";
 const indexRouter = Router();
 
 indexRouter.get("/", (req: Request, res: Response, next: NextFunction) => {
-  console.log(
-    "Hey 👋🏿. Welcome to the Software Essentialist Backend API project!"
-  );
   return res.send(
     "Hey 👋🏿. Welcome to the Software Essentialist Backend API project!"
   );
