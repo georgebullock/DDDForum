a0534a5a5d03fe1cfc32fcbc84d77e99f7c5163f
georgebullock <george.bullock@gmx.de>
2026-01-13 17:53:24 +0100

    📦 NEW:  Add edit user by ID route via asyncHandler. Include validation to confirm email, username, firstName, and lastName are not falsy.

--- 1_code_first/begin/backend/src/api/routes/userRoutes.ts
+++ 1_code_first/begin/backend/src/api/routes/userRoutes.ts
@@ -72,11 +72,69 @@ userRouter.post(
   })
 );
 
-userRouter.post("/edit/:userId ", (req: Request, res: Response) => {
-  const { id } = req.body;
+userRouter.post(
+  "/edit/:userId",
+  asyncHandler(async (req: Request, res: Response, _next: NextFunction) => {
+    const id = req.params.userId;
+    const { email, username, firstName, lastName } = req.body;
 
-  return res.send("Get user by ID");
-});
+    if (!email) {
+      throw new ApiError({
+        statusCode: 400,
+        error: "Validation Error: Email is required",
+        data: undefined,
+        success: false,
+      });
+    }
+
+    if (!username) {
+      throw new ApiError({
+        statusCode: 400,
+        error: "Validation Error: Username is required",
+        data: undefined,
+        success: false,
+      });
+    }
+
+    if (!firstName) {
+      throw new ApiError({
+        statusCode: 400,
+        error: "Validation Error: Firstname is required",
+        data: undefined,
+        success: false,
+      });
+    }
+
+    if (!lastName) {
+      throw new ApiError({
+        statusCode: 400,
+        error: "Validation Error: Lastname is required",
+        data: undefined,
+        success: false,
+      });
+    }
+
+    if (typeof id === "string") {
+      const data: User = await prisma.user.update({
+        where: {
+          id,
+        },
+        data: {
+          email,
+          username,
+          firstName,
+          lastName,
+        },
+      });
+
+      res.status(200).json({
+        error: undefined,
+        data,
+        success: true,
+      });
+    }
+  })
+);
 
 userRouter.get(
   "/",
