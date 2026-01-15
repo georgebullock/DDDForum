28ad1e11bb34272347abc17cb7e4ebc615b92ef7
georgebullock <george.bullock@gmx.de>
2026-01-15 15:55:59 +0100

    👌 IMPROVE: Add underscores to unused index route handler to clarify they are required by unused.

--- 1_code_first/begin/backend/src/api/routes/indexRoute.ts
+++ 1_code_first/begin/backend/src/api/routes/indexRoute.ts
@@ -2,7 +2,7 @@ import { Router, Request, Response, NextFunction } from "express";
 
 const indexRouter = Router();
 
-indexRouter.get("/", (req: Request, res: Response, next: NextFunction) => {
+indexRouter.get("/", (_req: Request, res: Response, _next: NextFunction) => {
   return res.send(
     "Hey 👋🏿. Welcome to the Software Essentialist Backend API project!"
   );
