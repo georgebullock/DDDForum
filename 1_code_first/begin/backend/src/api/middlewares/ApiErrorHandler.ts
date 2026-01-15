a6289442120ef25222defe7efdeee1ff3ad1fb2c
georgebullock <george.bullock@gmx.de>
2026-01-15 14:56:17 +0100

    👌 IMPROVE:  Remove redundant guard condition for Prisma errors

--- 1_code_first/begin/backend/src/api/middlewares/ApiErrorHandler.ts
+++ 1_code_first/begin/backend/src/api/middlewares/ApiErrorHandler.ts
@@ -13,24 +13,20 @@ const ApiErrorHandler = (
     res.status(statusCode).json({ error, data, success });
   }
 
-  if (err instanceof PrismaClientKnownRequestError) {
-    if (err instanceof PrismaClientKnownRequestError && err.code === "P2002") {
-      res.status(409).json({
-        error: `${err.meta?.target}AlreadyTaken`,
-        data: undefined,
-        success: false,
-      });
-    }
+  if (err instanceof PrismaClientKnownRequestError && err.code === "P2002") {
+    res.status(409).json({
+      error: `${err.meta?.target}AlreadyTaken`,
+      data: undefined,
+      success: false,
+    });
   }
 
-  if (err instanceof PrismaClientKnownRequestError) {
-    if (err instanceof PrismaClientKnownRequestError && err.code === "P2025") {
-      res.status(404).json({
-        error: `UserNotFound`,
-        data: undefined,
-        success: false,
-      });
-    }
+  if (err instanceof PrismaClientKnownRequestError && err.code === "P2025") {
+    res.status(404).json({
+      error: `UserNotFound`,
+      data: undefined,
+      success: false,
+    });
   }
 
   res
