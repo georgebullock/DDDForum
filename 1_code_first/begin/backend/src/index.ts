d6b18c8b3c9391fa8b7700676fc3b62f930ff63b
georgebullock <george.bullock@gmx.de>
2026-01-21 14:38:22 +0100

    📦 NEW: Add cors and its types. No configuration for now.

--- 1_code_first/begin/backend/src/index.ts
+++ 1_code_first/begin/backend/src/index.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import indexRouter from "./api/routes/indexRoute";
 import userRouter from "./api/routes/userRoutes";
 import ApiErrorHandler from "./api/middlewares/ApiErrorHandler";
@@ -7,6 +8,7 @@ const app = express();
 const port = 3000;
 
 app.use(express.json());
+app.use(cors());
 app.use("/", indexRouter);
 app.use("/users", userRouter);
 app.use(ApiErrorHandler);
