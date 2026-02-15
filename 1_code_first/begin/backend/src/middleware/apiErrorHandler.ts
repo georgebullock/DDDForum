d1c6aba496b8df30a5bc848f3e84228bae34e180
georgebullock <george.bullock@gmx.de>
2026-02-15 22:27:03 +0100

    🐛 FIX:  Add PrismaClientKnownRequestError to prevent known db errors from returning unknown server errors.

--- 1_code_first/begin/backend/src/middleware/apiErrorHandler.ts
+++ 1_code_first/begin/backend/src/middleware/apiErrorHandler.ts
@@ -1,5 +1,6 @@
 import { Request, Response, NextFunction } from "express";
 import ApiError from "../errors/ApiError";
+import errors from "../errors/errors";
 import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
 
 const apiErrorHandler = (
@@ -11,6 +12,22 @@ const apiErrorHandler = (
   if (err instanceof ApiError) {
     const { statusCode, error, data, success } = err.content;
     return res.status(statusCode).json({ error, data, success });
+  }
+
+  if (err instanceof PrismaClientKnownRequestError && err.code === "P2002") {
+    res.status(errors.applicationErrors.statusCode).json({
+      error: `${err.meta?.target}AlreadyTaken`,
+      data: undefined,
+      success: false,
+    });
+  }
+
+  if (err instanceof PrismaClientKnownRequestError && err.code === "P2025") {
+    res.status(errors.serverErrors.statusCode).json({
+      error: errors.applicationErrors.userNotFound,
+      data: undefined,
+      success: false,
+    });
   }
 
   res
