3f4f0333aa37dd4f6b718d8809d1444704485694
georgebullock <george.bullock@gmx.de>
2026-03-26 13:34:52 +0100

    docs: add form validation todos

--- 1_code_first/begin/frontend/src/Pages/RegisterPage.tsx
+++ 1_code_first/begin/frontend/src/Pages/RegisterPage.tsx
@@ -4,6 +4,20 @@ import MainContent from "../Layout/MainContent";
 import RegistrationForm from "../Components/RegistrationForm";
 
 function RegisterPage() {
+  // ToDo: If the form is invalid
+  // ToDo: Validate the form
+  // ToDo: Show an error toast (for invalid input)
+  // ToDo: If the form is valid, start isLoading
+  // ToDo: Make the API call
+  // ToDo: If the API call is successful
+  // ToDo: Save the user details to the cache
+  // ToDo: Stop the spinner
+  // ToDo: Show the toast
+  // ToDo: In 3 seconds, redirect to the main page
+  // ToDo: If the call failed
+  // ToDo: Stop the spinner
+  // ToDo: Show the toast (for unknown error)
+
   return (
     <PageLayout className="mx-5 my-10 flex flex-col md:max-w-3xl">
       <Header hasNav={false} className="flex flex-col justify-center gap-5" />
